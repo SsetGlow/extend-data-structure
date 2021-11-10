e8c1dd76ae2bcac8a511728c66bc5b0edb14ee20
ssetglow <ssetglow@outlook.com>
2021-11-10 19:51:53 +0800

    initial commit

---
+++ src/main/java/prvi/ssetglow/extenddatastructure/ExtendDataStructureApplication.java
@@ -0,0 +1,17 @@
+package prvi.ssetglow.extenddatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ssetglow
+ * @create 2021-11-10 19:46
+ */
+@SpringBootApplication
+public class ExtendDataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtendDataStructureApplication.class, args);
+    }
+
+}
