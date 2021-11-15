18a5c5bcf723a9247cdb6bc794c150ef1f987e93
ssetglow <ssetglow@outlook.com>
2021-11-15 09:23:00 +0800

    test(test): test

---
+++ src/main/java/prvi/ssetglow/extenddatastructure/random/Random.java
@@ -0,0 +1,13 @@
+package prvi.ssetglow.extenddatastructure.random;
+
+/**
+ * @program: extend-data-structure
+ * @packageName: prvi.ssetglow.extenddatastructure.random
+ * @className: Random
+ * @description:
+ * @author: SsetGlow
+ * @date: 2021-11-15 09:21
+ **/
+public class Random {
+
+}
---
+++ src/main/java/prvi/ssetglow/extenddatastructure/random/bean/RandomBean.java
@@ -0,0 +1,23 @@
+package prvi.ssetglow.extenddatastructure.random.bean;
+
+import java.math.BigDecimal;
+
+/**
+ * @program: extend-data-structure
+ * @packageName: prvi.ssetglow.extenddatastructure.random.bean
+ * @className: RandomBean
+ * @description:
+ * @author: SsetGlow
+ * @date: 2021-11-15 09:21
+ **/
+public class RandomBean {
+    private BigDecimal probability;
+
+    public BigDecimal getProbability() {
+        return probability;
+    }
+
+    public void setProbability(BigDecimal probability) {
+        this.probability = probability;
+    }
+}
