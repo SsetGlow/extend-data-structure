c328129ec6a109f05bf4cb5adf23c90fced31ef2
SsetGlow <ssetglow@outlook.com>
2021-12-02 18:17:18 +0800

    feat(BM): add bm algorithm

---
+++ src/main/java/prvi/ssetglow/extenddatastructure/string/Bm.java
@@ -0,0 +1,17 @@
+package prvi.ssetglow.extenddatastructure.string;
+
+/**
+ * @program: extend-data-structure
+ * @packageName: prvi.ssetglow.extenddatastructure.string
+ * @className: Bm
+ * @description:
+ * @author: SsetGlow
+ * @date: 2021-12-02 18:15
+ **/
+public class Bm {
+
+    public static int BmMatch(String source, String patternS) {
+        return -1;
+    }
+
+}
