a037fb3a199191a19f3963c1f86ae6c23804d942
SsetGlow <ssetglow@outlook.com>
2021-12-30 17:51:41 +0800

    feat(nim game): add bash game

---
+++ src/main/java/prvi/ssetglow/extenddatastructure/gametheory/NimGame.java
@@ -0,0 +1,21 @@
+package prvi.ssetglow.extenddatastructure.gametheory;
+
+import org.checkerframework.checker.tainting.qual.Untainted;
+import org.jetbrains.annotations.Contract;
+
+/**
+ * @program: extend-data-structure
+ * @packageName: prvi.ssetglow.extenddatastructure.gametheory
+ * @className: NimGame
+ * @description:
+ * @author: SsetGlow
+ * @date: 2021-12-30 17:10
+ **/
+public class NimGame {
+
+    @Untainted
+    @Contract(pure = true)
+    public boolean bashGame(int topStep, int totalCount) {
+        return totalCount % (topStep + 1) != 0;
+    }
+}
