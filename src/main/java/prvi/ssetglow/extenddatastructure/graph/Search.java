8b9d820e4bcae6157c86d5a1c45d676f03131077
SsetGlow <ssetglow@outlook.com>
2022-01-10 19:53:45 +0800

    feat(graph): add search function

---
+++ src/main/java/prvi/ssetglow/extenddatastructure/graph/Search.java
@@ -0,0 +1,20 @@
+package prvi.ssetglow.extenddatastructure.graph;
+
+/**
+ * @program: extend-data-structure
+ * @packageName: prvi.ssetglow.extenddatastructure.graph
+ * @className: Search
+ * @description:
+ * @author: SsetGlow
+ * @date: 2022-01-10 19:52
+ **/
+public class Search {
+
+    public void deepFirstSearch() {
+
+    }
+
+    public void breadthFirstSearch() {
+        
+    }
+}
