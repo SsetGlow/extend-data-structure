ddfd42c292ad228261e5921c40f17338ba674ac3
SsetGlow <ssetglow@outlook.com>
2022-11-03 16:40:57 +0800

    fix(annotation): remove :

--- src/main/java/priv/ssetglow/extenddatastructure/gametheory/NimGame.java
+++ src/main/java/priv/ssetglow/extenddatastructure/gametheory/NimGame.java
@@ -1,12 +1,12 @@
 package priv.ssetglow.extenddatastructure.gametheory;
 
 /**
- * @author: SsetGlow
- * @since: 0.1
- * @date: 2021-12-30 17:10
+ * @author SsetGlow
+ * @date 2021-12-30 17:10
+ * @since 0.1
  **/
 public class NimGame {
-    
+
     public boolean bashGame(int topStep, int totalCount) {
         return totalCount % (topStep + 1) != 0;
     }
