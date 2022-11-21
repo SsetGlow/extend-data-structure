def1fbc9ba3d429507dd5ccfedbae47205a678c3
SsetGlow <ssetglow@outlook.com>
2022-11-21 10:02:13 +0800

    fix(RandomBean): add default for RandomBean

--- src/main/java/priv/ssetglow/extenddatastructure/random/bean/RandomBean.java
+++ src/main/java/priv/ssetglow/extenddatastructure/random/bean/RandomBean.java
@@ -5,14 +5,15 @@ import org.jetbrains.annotations.NotNull;
 import java.math.BigDecimal;
 
 /**
- * @author: SsetGlow
- * @since: 0.1
- * @date: 2021-11-15 09:21
+ * @author SsetGlow
+ * @date 2021-11-15 09:21
+ * @since 0.1
  **/
 public class RandomBean {
     private @NotNull BigDecimal probability;
 
     public RandomBean() {
+        probability = BigDecimal.ONE;
     }
 
     public RandomBean(@NotNull BigDecimal probability) {
