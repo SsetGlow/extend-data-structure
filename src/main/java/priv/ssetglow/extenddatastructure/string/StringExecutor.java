e3b3ea1dc8b6d78b401a2ec6c23549159a44602c
SsetGlow <ssetglow@outlook.com>
2022-12-12 08:49:00 +0800

    feat(String): complete mancher

--- src/main/java/priv/ssetglow/extenddatastructure/string/StringExecutor.java
+++ src/main/java/priv/ssetglow/extenddatastructure/string/StringExecutor.java
@@ -1,6 +1,7 @@
 package priv.ssetglow.extenddatastructure.string;
 
 import org.checkerframework.checker.tainting.qual.Untainted;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -106,8 +107,54 @@ public class StringExecutor {
     }
 
     @NotNull
+    @Contract("_->new")
     private String manacher(@Untainted @NotNull String source) {
-        return "";
+        String T = preProcess(source);
+        int n = T.length();
+        int[] P = new int[n];
+        int C = 0, R = 0;
+        for (int i = 1; i < n - 1; i++) {
+            int i_mirror = 2 * C - i;
+            if (R > i) {
+                P[i] = Math.min(R - i, P[i_mirror]);// 防止超出 R
+            } else {
+                P[i] = 0;// 等于 R 的情况
+            }
+            // 碰到之前讲的三种情况时候，需要利用中心扩展法
+            while (T.charAt(i + 1 + P[i]) == T.charAt(i - 1 - P[i])) {
+                P[i]++;
+            }
+            // 判断是否需要更新 R
+            if (i + P[i] > R) {
+                C = i;
+                R = i + P[i];
+            }
+
+        }
+        // 找出 P 的最大值
+        int maxLen = 0;
+        int centerIndex = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (P[i] > maxLen) {
+                maxLen = P[i];
+                centerIndex = i;
+            }
+        }
+        int start = (centerIndex - maxLen) / 2; //最开始讲的求原字符串下标
+        return source.substring(start, start + maxLen);
     }
+
+    private String preProcess(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
+        }
+        StringBuilder ret = new StringBuilder("^");
+        for (int i = 0; i < n; i++)
+            ret.append("#").append(s.charAt(i));
+        ret.append("#$");
+        return ret.toString();
+    }
+
 
 }
