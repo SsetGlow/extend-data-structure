3ed5cf6b5143bea4a1ea738241d78a05f983b617
SsetGlow <ssetglow@outlook.com>
2023-01-09 10:06:30 +0800

    feat(AVLTree): add size when insert

--- src/main/java/priv/ssetglow/extenddatastructure/datastructure/tree/AvlTree.java
+++ src/main/java/priv/ssetglow/extenddatastructure/datastructure/tree/AvlTree.java
@@ -45,11 +45,13 @@ public class AvlTree<T extends Comparable<T>> extends BinaryTree<T> {
             }
             if (root.getLeftChild() == null) {
                 root.setLeftChild(node);
+                size++;
             } else if (root.getRightChild() == null) {
                 root.setRightChild(node);
+                size++;
             } else {
                 insert(node);
-                if (shouldBalance(node)) {
+                if (size >= 3 && shouldBalance(node)) {
                     balance(node);
                 }
             }
