5e5c087fc582bf02e0d3c3c49a960819778cb25f
SsetGlow <ssetglow@outlook.com>
2024-01-02 09:10:44 +0800

    feat(sort): add default sort func

---
+++ src/main/java/priv/ssetglow/extenddatastructure/baisc/SortExecutor.java
@@ -0,0 +1,24 @@
+package priv.ssetglow.extenddatastructure.baisc;
+
+import java.util.Collections;
+
+/**
+ * @author SsetGlow
+ * @date 2024-01-02 周二 09:08
+ * @since 1.0.0
+ **/
+public class SortExecutor {
+
+    public <T extends Collections> void bubbleSort(T data) {
+
+    }
+
+    public <T extends Collections> void selectSort(T data) {
+
+    }
+
+    public <T extends Collections> void insertSort(T data) {
+        
+    }
+
+}
