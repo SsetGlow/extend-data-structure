483e6d135da9b6f1c3a789f85850ce78dc0bc000
SsetGlow <ssetglow@outlook.com>
2024-01-04 17:00:46 +0800

    fix(constant): add static import

--- src/main/java/priv/ssetglow/extenddatastructure/bitmask/BitMask.java
+++ src/main/java/priv/ssetglow/extenddatastructure/bitmask/BitMask.java
@@ -3,10 +3,11 @@ package priv.ssetglow.extenddatastructure.bitmask;
 import org.checkerframework.checker.tainting.qual.Untainted;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-import priv.ssetglow.extenddatastructure.common.Constants;
 import priv.ssetglow.extenddatastructure.math.QuickPow;
 
 import java.math.BigInteger;
+
+import static priv.ssetglow.extenddatastructure.common.Constants.*;
 
 /**
  * @author SsetGlow
@@ -29,7 +30,7 @@ public class BitMask {
         int[] masks = new int[words.length];
         for (int i = 0; i < words.length; ++i) {
             for (int j = 0; j < words[i].length(); ++j) {
-                masks[i] |= 1 << (words[i].charAt(j) - Constants.SMALL_A.charValue);
+                masks[i] |= 1 << (words[i].charAt(j) - SMALL_A.charValue);
             }
         }
         for (int i = 1; i < masks.length; ++i) {
@@ -49,10 +50,10 @@ public class BitMask {
     private static boolean hasDuplicateWord(@NotNull @Untainted String s1, @NotNull @Untainted String s2) {
         int[] masks = new int[2];
         for (int i = 0; i < s1.length(); ++i) {
-            masks[0] |= 1 << (s1.charAt(i) - Constants.SMALL_A.charValue);
+            masks[0] |= 1 << (s1.charAt(i) - SMALL_A.charValue);
         }
         for (int i = 0; i < s2.length(); ++i) {
-            masks[1] |= 1 << (s2.charAt(i) - Constants.SMALL_A.charValue);
+            masks[1] |= 1 << (s2.charAt(i) - SMALL_A.charValue);
         }
         return (masks[0] & masks[1]) != 0;
     }
@@ -63,7 +64,7 @@ public class BitMask {
             return 0;
         }
         int n = 1;
-        while (QuickPow.quickPow(Constants.TWO.numberValue, n).compareTo(BigInteger.valueOf(glassCount)) < 0) ++n;
+        while (QuickPow.quickPow(TWO.numberValue, n).compareTo(BigInteger.valueOf(glassCount)) < 0) ++n;
         return n;
     }
 
@@ -90,7 +91,7 @@ public class BitMask {
         char[] chars = new char[s1.length()];
         for (int i = 0, j = 0; i < s1.length(); ++i) {
             char c = s1.charAt(i);
-            if (Constants.NEGATIVE_ONE.numberValue != s2.indexOf(c)) chars[j++] = c;
+            if (NEGATIVE_ONE.numberValue != s2.indexOf(c)) chars[j++] = c;
         }
         return chars;
     }
