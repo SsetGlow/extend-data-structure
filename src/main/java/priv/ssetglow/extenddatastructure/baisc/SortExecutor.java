1026e0a6fb526a1aa53b1c2f29b5147ac9a805a8
SsetGlow <ssetglow@outlook.com>
2024-01-05 18:51:22 +0800

    feat(sort): add select sort 1

--- src/main/java/priv/ssetglow/extenddatastructure/baisc/SortExecutor.java
+++ src/main/java/priv/ssetglow/extenddatastructure/baisc/SortExecutor.java
@@ -18,15 +18,11 @@ public class SortExecutor {
     public void selectSort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = i;
-            //把当前遍历的数和后面所有的数依次进行比较，并记录下最小的数的下标
             for (int j = i + 1; j < data.length; j++) {
-                //如果后面比较的数比记录的最小的数小
                 if (data[j] < data[min]) {
-                    //记录最小的那个数的下标
                     min = j;
                 }
             }
-            //如果最小的数和当前遍历数的下标不一致，说明下标为min的数比当前遍历的数更小
             if (i != min) {
                 change(data[i], data[min]);
             }
